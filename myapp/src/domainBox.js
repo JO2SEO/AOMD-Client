24af55a8144b6907055c5c723971daa9ea1a3917
zwon2056 <zwon2056@gmail.com>
2022-10-27 10:17:09 +0900

    ADD: 10/27 - 찐막 찐막 찐막 커밋

--- myapp/src/domainBox.js
+++ myapp/src/domainBox.js
@@ -1,4 +1,4 @@
-export const KakaoRedirectURL = 'http://20.249.85.3:32411/api/v1/auth/kakao';
+export const KakaoRedirectURL = 'http://20.249.13.138:32411/api/v1/auth/kakao';
 export const ServerURL_port = 'http://20.249.13.138:32411/api/v1/portfolio';
 export const ServerURL_block = 'http://20.249.13.138:32411/api/v1/portfolio/block';
 export const ServerURLLogin = 'http://20.249.13.138:32411/api/v1/auth/login';
